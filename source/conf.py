fd67c7dc827ab9413dd5be8ceb08ab41ddb581cd
Daltz333 <darasmith@wccnet.edu>
2019-04-05 23:32:08 -0400

    Added necessary requirements for ReadTheDocs and Markdown

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,7 @@ author = 'FIRST Community'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +43,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
