b7c1cca811d6c820051163da1df2aea47c5a8192
Dalton Smith <darasmith@wccnet.edu>
2019-09-14 11:09:17 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ todo_include_todos = False
 linkcheck_anchors = False
 
 # Linkcheck Exclusions
-linkcheck_ignore = [r'kauailabs.com']
+linkcheck_ignore = [r'/kauailabs.com/g']
 
 # Autosection labels prefix document path and filename
 autosectionlabel_prefix_document = True
