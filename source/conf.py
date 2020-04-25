1483a34213b4f73828577a8cc8ea5659167d7c29
Dalton Smith <darasmith@wccnet.edu>
2020-04-25 12:11:25 -0400

    Orphan machine learning tutorial (#636)
    * Revert "Hide the machine learning tutorial until it's fixed (#589)"
    
    This reverts commit 93d7edff6a02906b08715ab1e574174b4b571040.
    
    * Orphanize the ML tutorial

--- source/conf.py
+++ source/conf.py
@@ -65,7 +65,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['.png', '.jpg', 'docs/beta/*', 'docs/software/examples-tutorials/machine-learning/*']
+exclude_patterns = ['.png', '.jpg', 'docs/beta/*']
 
 # Specify the master doc file, AKA our homepage
 master_doc = "index"
