27ba38f223547424f94a198d3c9518fe0121afb0
Itay Ziv <itay220204@gmail.com>
2020-05-24 18:54:49 +0300

    Add image file size check

---
+++ scripts/imagesizechecker.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+
+IMAGE_FORMATS = ('.png', '.jpg', '.jpeg', '.svg')
+KILOBYTE_SIZE = 1000
+
+
+def verify_image_size(file, max_size):
+    if file.path.endswith(IMAGE_FORMATS):
+        file_stat = file.stat()
+        size_valid = file_stat.st_size <= max_size
+        print(file.path + "  ", end="")
+        print("OK" if size_valid else "FILE SIZE TOO LARGE", end="")
+        print(" File Size: " + str(file_stat.st_size))
+
+        return size_valid
+    return True
+
+
+def iterate_image_sizes(path, max_size):
+    oversized_count = 0
+    for entry in os.scandir(path):
+        if entry.is_file():
+            if verify_image_size(entry, max_size):
+                oversized_count += 1
+        elif entry.is_dir():
+            oversized_count += iterate_image_sizes(entry.path, max_size)
+    return oversized_count
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(description="Verifies image file size is valid")
+    arg_parser.add_argument("path", type=str, help="The path to scan in")
+    arg_parser.add_argument("max_size", type=int, help="The max size of a file in kilobytes")
+
+    args = vars(arg_parser.parse_args())
+
+    oversized_count = iterate_image_sizes(args["path"], args["max_size"] * KILOBYTE_SIZE)
+
+    if oversized_count == 0:
+        print("\nfile sizes valid.")
+    else:
+        raise(Exception("\n{} files are too large.".format(oversized_count)))
+
+
+if __name__ == '__main__':
+    main()
