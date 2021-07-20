36d031519c71801ed12b1993e1ebd58be6fae781
Daltz333 <daltzsmith@gmail.com>
2021-07-20 00:00:25 -0400

    Remove debug statement

--- source/sphinxext/minify.py
+++ source/sphinxext/minify.py
@@ -51,10 +51,8 @@ class ImageMinifier(ImageConverter):
                     if is_image_a_vi_snippet(_from):
                         return False
 
-                logger.info("Compressing Image")
-
                 image.save(_to, quality=self.config.minify_image_quality, optimize=True)
-                
+
             return True
         except OSError:
             logger.warning(__("Unable to convert file %r, " "please verify!"), _from)
