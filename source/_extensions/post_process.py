147d52d20e699fae2f2f68d800bd938bb30888b2
Vasista Vovveti <vasistavovveti@gmail.com>
2021-10-09 15:20:50 -0500

    Add post processing and minify fontawesome (#1430)
    * Add post processing
    - minify html, css, js
    - minify fontawesome css usage
    - minify fontawesome font files
    
    * disable html minifying
    
    * disable all minifying
    
    * Remove broken code
    
    * remove random unused function
    
    * Remove conf.py declarations
    
    * Update dependencies
    
    * Add css cleanup back in
    
    * Fix font subsetting
    
    * reorganize files
    
    Co-authored-by: Daltz333 <daltzsmith@gmail.com>

---
+++ source/_extensions/post_process.py
@@ -0,0 +1,42 @@
+# This file is the final sphinx extension that runs after a build is finished.
+# Everything scheduled in do() is run sequentially and may need order preserved.
+# Sphinx extensions can specify their execution priority but it's cumbersome to
+# increment a value across files.
+
+from typing import Any, Dict, Union
+
+from sphinx.application import Sphinx
+from sphinx.builders.dirhtml import DirectoryHTMLBuilder
+from sphinx.builders.html import StandaloneHTMLBuilder
+
+from .post_process_tasks.fontawesome import (
+    cleanup_fontawesome_css,
+    cleanup_fontawesome_font_files,
+)
+
+READTHEDOCS_BUILDERS = ["readthedocs", "readthedocsdirhtml"]
+
+
+def do(app: Sphinx, exception: Union[Exception, None]) -> None:
+    if exception:
+        return
+    if not (
+        isinstance(app.builder, (StandaloneHTMLBuilder, DirectoryHTMLBuilder))
+        or app.builder.name in READTHEDOCS_BUILDERS
+    ):
+        return
+
+    print("Running custom post processing")
+
+    # The two fontawesome functions must run in order
+    cleanup_fontawesome_css(app)
+    cleanup_fontawesome_font_files(app)
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+    app.connect("build-finished", do, float("inf"))
+
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sys
+import os
+
+sys.path.append(os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 
@@ -48,6 +52,12 @@ extensions = [
     "versionwarning.extension",
     "sphinx_panels",
 ]
+
+local_extensions = [
+    "_extensions.post_process",
+]
+
+extensions += local_extensions
 
 versionwarning_messages = {
     "latest": """
