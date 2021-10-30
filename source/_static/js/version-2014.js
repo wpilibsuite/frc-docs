a45be10f599b6c8829a944422ebc2b5c2505c112
Vasista Vovveti <vasistavovveti@gmail.com>
2021-10-30 18:50:08 -0500

    Formatting

--- source/_static/js/version-2014.js
+++ source/_static/js/version-2014.js
@@ -1,24 +1,22 @@
 $lazy = async function (selector) {
-	let $this = [];
-	
-	while (!$this.length) {
-        await new Promise(resolve => setTimeout(resolve, 500));
-		$this = $(selector);
-	}
-	
-	return $this;
-	
-}
-	
+    let $this = [];
+
+    while (!$this.length) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        $this = $(selector);
+    }
+
+    return $this;
+};
 
 document.addEventListener(
     "DOMContentLoaded",
     async function () {
-      (await $lazy(".rst-other-versions dt:contains('Versions')"))
-        .parent()
-        .append(
-          `<dd><a href="https://wpilib.screenstepslive.com/s/3120">2014</a></dd>`
-        );
+        (await $lazy(".rst-other-versions dt:contains('Versions')"))
+            .parent()
+            .append(
+                `<dd><a href="https://wpilib.screenstepslive.com/s/3120">2014</a></dd>`
+            );
     },
     false
 );
