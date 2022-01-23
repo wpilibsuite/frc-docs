115a0390881dfc36f5437a3daaa7d4d7524f6c63
TheTripleV <vasistavovveti@gmail.com>
2022-01-23 17:21:25 -0600

    Change rtd builder superclass

---
+++ source/_extensions/rtd_patch.py
@@ -0,0 +1,16 @@
+# Force RTD to build zip with regular html builder, not single html
+# The builder is preloaded after all extensions are loaded, so we can
+# change the registered class here.
+
+try:
+    from sphinx.builders.html import StandaloneHTMLBuilder
+except ImportError:
+    from sphinx.builders.html import StandaloneHTMLBuilder
+
+
+class ReadtheDocsBuilderLocalMedia(StandaloneHTMLBuilder):
+    name = "readthedocssinglehtmllocalmedia"
+
+
+def setup(app):
+    app.add_builder(ReadtheDocsBuilderLocalMedia, override=True)
