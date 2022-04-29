b0670af447d64a9786acc7f2c3c6c08e35450e7c
Daltz333 <daltzsmith@gmail.com>
2022-04-29 19:15:46 -0400

    Readd baseurl declaration

--- source/conf.py
+++ source/conf.py
@@ -197,6 +197,10 @@ pwa_maskable_icon = "first-logo-maskable.png"
 #
 html_theme = "sphinx_rtd_theme"
 
+# Specify canonical root
+# This tells search engines that this domain is preferred
+html_baseurl = "https://docs.wpilib.org/en/stable/"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
