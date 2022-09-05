8372ff6105041ba6d047d2c1b97c3a288c944dc7
Chris Gerth <chrisgerth010592@gmail.com>
2022-09-05 15:19:29 -0500

    retrigger CI

--- source/_static/js/pid-tune/1-delay-line.js
+++ source/_static/js/pid-tune/1-delay-line.js
@@ -1,19 +1,16 @@
 class DelayLine
 {
-    constructor(num_samples)
-    {
+    constructor(num_samples) {
         this.items = [];
         this.desLen = num_samples;
     }
     
-    addSample(val)
-    {
+    addSample(val) {
         this.items.push(val)
         this.num_samples++;
     }
 
-    getSample()
-    {
+    getSample() {
         if(this.items.length >= this.desLen){
             return this.items.shift()
         } else {
