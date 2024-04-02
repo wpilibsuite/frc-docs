ba7fdfd3527f81c104adbe7e9034419c52f17a79
GrahamSH <grahamshllk@gmail.com>
2024-04-02 12:27:59 +0000

    sidebar caption color

--- source/conf.py
+++ source/conf.py
@@ -225,7 +225,7 @@ html_theme_options = {
    # "collapse_navigation": True,
   #  "sticky_navigation": False,
    # "titles_only": True,
-    "light_css_variables": {"color-sidebar-background": "#003974", "color-sidebar-brand-text": "#ffffffcc", "color-sidebar-link-text--top-level": "#d9d9d9","color-background-hover": "#023160","color-sidebar-link-text":"#acb1b9"}
+    "light_css_variables": {"color-sidebar-background": "#003974", "color-sidebar-brand-text": "#ffffffcc", "color-sidebar-link-text--top-level": "#d9d9d9","color-background-hover": "#023160","color-sidebar-link-text":"#acb1b9","color-sidebar-caption-text":"#81868d"}
 }
 
 user_options = [
