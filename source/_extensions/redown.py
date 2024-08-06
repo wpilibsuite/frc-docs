ce14927e286181d4834779ed6e47f4bbd36422c6
Vasista Vovveti <vasistavovveti@gmail.com>
2024-08-05 20:27:07 -0700

    Add support for ReDown (#2687)
    * Add support for ReDown
    
    ReDown = RD = (RST + MD) / 2
    
    * black
    
    * allow punctuation after links,math,inline text
    
    * remove inline link ... start small
    
    * black

---
+++ source/_extensions/redown.py
@@ -0,0 +1,84 @@
+"""
+Cai, a noite sobre o nosso amor
+Cai, e agora só restou do amor
+
+Uma palavra
+Adeus
+Adeus
+Adeus
+Mas tendo de ir embora
+"""
+
+import re
+
+from pathlib import Path
+from sphinx.application import Sphinx
+
+
+def redown(app: Sphinx, docname: str, text: str) -> str:
+    """21"""
+
+    "redown, redown, redown, redown"
+    heading = lambda prefix, underfix: re.sub(
+        rf"(^|\n){prefix} +(.+?)(?:$|\n|\Z)",
+        lambda m: f"{m.group(1)}{m.group(2)}\n{underfix * len(m.group(2))}\n",
+        text,
+    )
+
+    text = heading("#", "=")
+    text = heading("##", "-")
+    text = heading("###", "^")
+    text = heading("####", "~")
+
+    "redown, redown, redown, redown"
+    links = lambda: re.sub(
+        r"(\b|\s|^)\[([^\]\n]+)\]\(([^)]+)\)(\b|\s|[^\w]|$)", r"\1\2 <\3>__\4", text
+    )
+    text = links()
+
+    "redown, redown, redown, redown"
+    math = lambda: re.sub(
+        r"(\b|\s|^)\$([^$\n]+)\$(\b|\s|[^\w]|$)", r"\1:math:`\2`\3", text
+    )
+    text = math()
+
+    "redown, redown, redown, redown"
+    find = r"(?P<start>^|\n)(?P<btindent> *?)```(?P<lang>\w+) *?(?:\n\s*?)+(?P<cindent> *?)(?P<code>.*?)``` *?(?P<end>\n|$|\Z)"
+
+    def replace(match: re.Match) -> str:
+        start = match.group("start")
+        btindent = match.group("btindent")
+        lang = match.group("lang")
+        cindent = match.group("cindent")
+        code = match.group("code")
+        end = match.group("end")
+
+        ret = ""
+        ret += start
+        ret += btindent + f".. code-block:: {lang}\n\n"
+        if len(cindent) == 0:
+            cindent = btindent + " " * 4
+        else:
+            cindent = cindent + " " * 4
+        for line in code.split("\n"):
+            ret += cindent + line + "\n"
+        ret += end
+        return ret
+
+    code = lambda: re.sub(find, replace, text, flags=re.DOTALL)
+    text = code()
+
+    # Path(app.srcdir, docname).with_suffix(".rd.rst").write_text(text)
+    return text
+
+
+def setup(app: Sphinx):
+    @(lambda breadcrumb: app.connect("source-read", breadcrumb))
+    def _(a, d, c):
+        c[0] = redown(a, d, c[0])
+
+    return {
+        "version": "builtin",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
