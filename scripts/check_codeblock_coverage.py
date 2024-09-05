99a21178c9170501dedc1a4161c3047e4bed2ae5
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-09-05 17:50:23 -0400

    Working version 1

--- scripts/check_codeblock_coverage.py
+++ scripts/check_codeblock_coverage.py
@@ -1,4 +1,4 @@
-import sys, os, re
+import sys, os, re, argparse
 from dataclasses import dataclass
 
 @dataclass
@@ -6,7 +6,6 @@ class CodeBlock:
     start: int
     file: str
     langs: list[str]
-    block: str
 
 
 
@@ -23,45 +22,64 @@ def is_codeblock(line: str):
     return line.startswith(".. tab-set-code::") or line.startswith(".. tab-set::")
 
 def get_blocks_from_rst_file(file: str):
-    lang_regex = re.compile("(java|python|cpp)")
     blocks = []
     with(open(file, 'r', encoding='utf8')) as f:
-        block_text = ''
-        found_block = False
         block_start = None
         langs = []
         for index, line in enumerate(f):
             if is_codeblock(line):
-                found_block = True
-                block_text += line
-                block_start = index + 1
-            elif found_block:
-                if line.startswith(' ') or line.startswith('\t'):
-                    lang = re.search("(`{3}|:language: )(java|python|cpp|c\\+\\+)", line)
-                    if(lang is not None):
-                        langs.append(lang.group(2))
-                    block_text += line
-                else:
-                    if langs != []:        
+                if langs != []:        
                         blocks.append(
                             CodeBlock(
-                                block=block_text,
                                 start=block_start,
                                 file=file,
                                 langs=langs
                                 )
                             )
 
-                    block_text = ''
-                    block_start = None
-                    found_block = False
-                    langs = []
+                block_start = index + 1
+                langs = []
+            else:
+                if line.startswith(' ') or line.startswith('\t'):
+                    lang = re.search("(`{3}|:language: )(java|python|c\\+\\+)", line.lower())
+                    if(lang is not None):
+                        langs.append(lang.group(2))
     return blocks
 
+def generate_report(blocks: list[CodeBlock], langs: list[str], wordy: bool):
+    stream = sys.stdout
+    if wordy:
+        stream = open("output.txt", "w")
+
+    blocks_count = len(blocks)
+    langs_coverage = {lang: 0 for lang in langs}
+    for block in blocks:
+        for lang in langs:
+            if lang in block.langs:
+                langs_coverage[lang] += 1
+    print(f"Total code blocks: {blocks_count}", file=stream)
+    for lang, coverage in langs_coverage.items():
+        print(f"{lang} coverage: {coverage}/{blocks_count} ({coverage/blocks_count*100:.2f}%)", file=stream)
+
+    if wordy:
+        print("\n\nMissing code blocks:", file=stream)
+        for block in blocks:
+            missing_langs = [lang for lang in langs if lang not in block.langs]
+            if missing_langs:
+                print(f"File: {block.file}, Line: {block.start}", file=stream)
+                print(f"Missing languages: {missing_langs}", file=stream)
+
+        stream.close()
+
 def main():
-    # dir = sys.argv[1]
-    dir = "source"
-    files = get_all_rst_files(dir=dir)
+    parser = argparse.ArgumentParser(description='Check code block coverage in FRC docs')
+    parser.add_argument('--dir', type=str, help='Directory to search for rst files')
+    parser.add_argument('--wordy', action='store_true', help='Outputs which code blocks are missing languages')
+
+    args = parser.parse_args()
+    print(args.wordy)
+
+    files = get_all_rst_files(dir=args.dir)
     blocks = []
     for file in files:
         file_blocks = get_blocks_from_rst_file(file)
@@ -69,9 +87,9 @@ def main():
             continue
         else: 
             blocks.extend(file_blocks)
-    with(open("output.txt", 'w')) as f:
-                for block in blocks:
-                    f.write(f"Block: {block}\n")
+    generate_report(blocks=blocks, langs=["java", "python", "c++"], wordy=args.wordy)
+
+
 
 if __name__ == '__main__':
     main()
