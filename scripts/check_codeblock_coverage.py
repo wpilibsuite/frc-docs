e4e0939789d4ce697c6dd0b567adaecd461924f0
Europa <falon.n.clark@gmail.com>
2024-09-05 16:52:16 -0400

    Update check_codeblock_coverage.py

--- scripts/check_codeblock_coverage.py
+++ scripts/check_codeblock_coverage.py
@@ -1,11 +1,13 @@
-import sys, os
+import sys, os, re
 from dataclasses import dataclass
 
 @dataclass
 class CodeBlock:
-    block: str
     start: int
     file: str
+    langs: list[str]
+    block: str
+
 
 
 def get_all_rst_files(dir):
@@ -17,41 +19,58 @@ def get_all_rst_files(dir):
 
     return files
 
-def get_blocks_from_rst_file(file):
+def is_codeblock(line: str):
+    return line.startswith(".. tab-set-code::") or line.startswith(".. tab-set::")
+
+def get_blocks_from_rst_file(file: str):
+    lang_regex = re.compile("(java|python|cpp)")
     blocks = []
-    with(open(file, 'r')) as f:
+    with(open(file, 'r', encoding='utf8')) as f:
         block_text = ''
         found_block = False
         block_start = None
-        for linenum, line in enumerate(f):
-            if line.startswith(".. tab-set-code::"):
+        langs = []
+        for index, line in enumerate(f):
+            if is_codeblock(line):
                 found_block = True
                 block_text += line
-                block_start = linenum
+                block_start = index + 1
             elif found_block:
                 if line.startswith(' ') or line.startswith('\t'):
+                    lang = re.search("(`{3}|:language: )(java|python|cpp|c\+\+)", line)
+                    if(lang is not None):
+                        langs.append(lang.group(2))
                     block_text += line
                 else:
-                    blocks.append(CodeBlock(block=block_text, start=block_start, file=file))
+                    if langs != []:        
+                        blocks.append(
+                            CodeBlock(
+                                block=block_text,
+                                start=block_start,
+                                file=file,
+                                langs=langs
+                                )
+                            )
+
                     block_text = ''
                     block_start = None
                     found_block = False
     return blocks
 
 def main():
-    dir = sys.argv[1]
+    # dir = sys.argv[1]
+    dir = "source"
     files = get_all_rst_files(dir=dir)
+    blocks = []
     for file in files:
-        blocks = get_blocks_from_rst_file(file)
-        if len(blocks) == 0: continue
+        file_blocks = get_blocks_from_rst_file(file)
+        if len(file_blocks) == 0: 
+            continue
         else: 
-            with(open("output.txt", 'w')) as f:
+            blocks.extend(file_blocks)
+    with(open("output.txt", 'w')) as f:
                 for block in blocks:
-                    f.write(f"File: {block.file}\n")
-                    f.write(f"Block: {block.block}\n")
-                    f.write(f"Start: {block.start}\n")
-                    f.write("\n")
-                break
+                    f.write(f"Block: {block}\n")
 
 if __name__ == '__main__':
     main()
