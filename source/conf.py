f18ca0fa25adebb47416d1de7032149f4d10a001
sciencewhiz <sciencewhiz@users.noreply.github.com>
2024-10-13 23:12:31 -0700

    Enable beta docs for 2025 (#2786)

--- source/conf.py
+++ source/conf.py
@@ -178,7 +178,6 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
     "docs/yearly-overview/2020-Game-Data.rst",
-    "docs/beta/*",
 ]
 
 # Specify the master doc file, AKA our homepage
