5ef38bce45dcf1004695d5090028ee6d9dbea729
sciencewhiz <sciencewhiz@users.noreply.github.com>
2026-05-02 13:18:08 -0700

    Improve redown error handling
    Ensure the redown debug files are written UTF-8
    Fix one instance of smart quotes

--- scripts/doc8_redown.py
+++ scripts/doc8_redown.py
@@ -8,11 +8,15 @@ from redown import redown
 
 old_setattr = parser.ParsedFile.__setattr__
 
-
 def new_setattr(self, name, value):
     if name == "_raw_content" and value is not None:
-        value = redown(value.decode("utf-8").replace("\r", "")).encode("utf-8")
-        # Path(self.filename).with_suffix(".rd").write_text(value.decode("utf-8"))
+        current_filename = self.filename
+        try:
+            value = redown(value.decode("utf-8").replace("\r", "")).encode("utf-8")
+            # Path(self.filename).with_suffix(".rd").write_text(value.decode("utf-8"), encoding='utf-8')
+        except Exception as e:
+            print(f"Error processing file {self.filename}: {e}", file=sys.stderr)
+            raise
     old_setattr(self, name, value)
 
 
